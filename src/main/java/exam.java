bf5272f36377cd6edc7e7daca5cd26ca9ef23908
sumin <lsms0523@gmail.com>
2023-03-08 12:38:57 +0900

    Initial commit

---
+++ src/main/java/exam.java
@@ -0,0 +1,19 @@
+public class exam {
+    public static void main(String[] args) {
+
+        System.out.println("hello world");
+        System.out.println();
+        System.out.println();
+        System.out.println("asdf");
+        System.out.println("asdasdads");
+        System.out.println("asasas");
+        System.out.println("aaaa");
+        System.out.println("asdfsadf");
+
+        for( int i = 0; i < 10; i++) {
+        }
+
+        return;
+
+    }
+}

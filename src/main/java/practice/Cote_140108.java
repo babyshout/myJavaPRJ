5c1f2b746866e7a833efa719a21a28181caf258d
home <lsms2004@naver.com>
2023-03-11 17:09:06 +0900

    COMMIT

---
+++ src/main/java/practice/Cote_140108.java
@@ -0,0 +1,46 @@
+package practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+
+        System.out.println(s);
+
+        for (char ch :
+                s.) {
+            
+        }
+
+
+
+        return answer;
+    }
+}
+
+public class Cote_140108 {
+
+    public static void main(String[] args) {
+
+
+        List<String> listStr = new ArrayList<>();
+
+        listStr.add("banana");
+        listStr.add("abracadabra");
+        listStr.add("aaabbaccccabba");
+
+        for (String str :
+                listStr) {
+
+            Solution solution = new Solution();
+
+            System.out.println(solution.solution(str));
+
+        }
+        
+    }
+
+
+}
---
+++ src/main/java/practice/StringDemo.java
@@ -0,0 +1,37 @@
+package practice;
+
+import java.util.ArrayList;
+
+public class StringDemo {
+
+//    void soloution() {
+//
+//        ArrayList<String>() arrayListString = new ArrayList<>();
+//
+//        String str1 = "12345";
+//
+//        System.out.println(str1);
+//
+//        String str2[] = str1.split("3");
+//
+//        System.out.println(str1);
+//
+//        System.out.println(str2);
+//
+//        System.out.println(str2.length);
+//
+//        System.out.println(str2[0]);
+//        System.out.println(str2[1]);
+//        System.out.println(str2[2]);
+//
+//    }
+
+    public static void main(String[] args) {
+
+//        StringDemo stringDemo = new StringDemo();
+//
+//        stringDemo.soloution();
+
+    }
+
+}

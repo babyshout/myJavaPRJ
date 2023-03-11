799a0fda4a64b504bec1830e53766b4a244f08eb
home <lsms0523@gmail.com>
2023-03-11 18:00:50 +0900

    COMMIT

--- src/main/java/practice/StringDemo.java
+++ src/main/java/practice/StringDemo.java
@@ -4,33 +4,33 @@ import java.util.ArrayList;
 
 public class StringDemo {
 
-//    void soloution() {
-//
-//        ArrayList<String>() arrayListString = new ArrayList<>();
-//
-//        String str1 = "12345";
-//
-//        System.out.println(str1);
-//
-//        String str2[] = str1.split("3");
-//
-//        System.out.println(str1);
-//
-//        System.out.println(str2);
-//
-//        System.out.println(str2.length);
-//
-//        System.out.println(str2[0]);
-//        System.out.println(str2[1]);
+    void soloution() {
+
+
+        String str1 = "12345";
+
+        System.out.println(str1);
+
+        String str2[] = str1.split("3");
+
+        System.out.println(str1);
+
+        System.out.println(str2);
+
+        System.out.println(str2.length);
+
+        System.out.println(str2[0]);
+        System.out.println(str2[1]);
 //        System.out.println(str2[2]);
-//
-//    }
+
+
+    }
 
     public static void main(String[] args) {
 
-//        StringDemo stringDemo = new StringDemo();
-//
-//        stringDemo.soloution();
+        StringDemo stringDemo = new StringDemo();
+
+        stringDemo.soloution();
 
     }
 

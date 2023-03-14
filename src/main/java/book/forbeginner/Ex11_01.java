ac0913174ddb54080e2be2b66b491f7540482bf3
sumin <lsms0523@gmail.com>
2023-03-14 15:18:31 +0900

    Initial commit

---
+++ src/main/java/book/forbeginner/Ex11_01.java
@@ -0,0 +1,78 @@
+package book.forbeginner;
+
+class Car {
+
+    private String color;
+    private int speed = 0;
+
+    Car() {
+        color = "led";
+        speed = 0;
+    }
+
+    Car(String color, int speed) {
+        this.color = color;
+        this.speed = speed;
+    }
+
+    protected void setColor(String color){
+        this.color = color;
+    }
+
+    void colorPrint () {
+        System.out.printf("Color is " + color + "\n");
+    }
+
+
+    void speedUp(int value){
+        if (speed + value > 200)
+            speed = 200;
+        else
+            speed = speed + value;
+
+        System.out.println("speed = " + speed);
+
+    }
+
+
+    void speedDown(int value) {
+        if (speed - value < 0)
+            speed = 0;
+        else
+            speed = speed - value;
+
+        System.out.println("speed = " + speed);
+
+    }
+
+    void speedPrint() {
+        System.out.printf("speed is " + speed + "\n");
+    }
+
+}
+public class Ex11_01 {
+
+    public static void main(String args[]){
+
+        Car myCar1 = new Car();
+        Car myCar2 = new Car();
+        Car myCar3 = new Car();
+
+        myCar1.speedUp(30);
+        myCar1.speedPrint();
+        myCar1.colorPrint();
+
+        myCar1.setColor("red");
+        myCar1.colorPrint();
+
+        myCar2.speedUp(50);
+        myCar2.speedPrint();
+
+        myCar3.speedUp(80);
+        myCar3.speedPrint();
+
+
+        return;
+    }
+
+}
---
+++ src/main/java/practice/star/StarPlusMinus.java
@@ -0,0 +1,144 @@
+package practice.star;
+
+public class StarPlusMinus {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+
+        for (int i = 0; i < a * 2; i++) {
+
+            if (i < a) {
+                for (int j = 0; j < a; j++) {
+                    if (a - i <= j)
+                        System.out.printf("*");
+                    else
+                        System.out.printf(" ");
+                }
+
+            } else {
+                for (int j = a; j > 0; j--) {
+                    if (i - a < j)
+                        System.out.printf("*");
+                    else
+                        System.out.printf(" ");
+                }
+
+            }
+            System.out.printf("\n");
+        }
+
+//        >
+
+//        for (int i = 0; i < a * 2; i++) {
+//            if (i < a) {
+//                for (int j = 0; j < i; j++) {
+//                    System.out.printf("*");
+//                }
+//            } else {
+//                for (int j = a; j > i - a; j--) {
+//                    System.out.printf("*");
+//                }
+//            }
+//            System.out.printf("\n");
+//        }
+
+//        int b = 7;
+//        int c = 0;
+//        for (int i = 0; i < b; i++) {
+//            c += i;
+//            // c = c + i;
+//        }
+//
+//        System.out.println(c);
+
+//        int c = 0;
+//        int b = 0;
+//
+//        while(c <= 10){
+//
+//            b += c;
+//
+//            c++;
+//
+//
+//        }
+//
+//        System.out.printf("%d", b);
+
+
+
+//        <
+
+//        for (int i = 0; i < a * 2; i++) {
+//
+//            if (i < a) {
+//                for (int j = 0; j < a; j++) {
+//                    if (i > j)
+//                        System.out.printf(" ");
+//                    else
+//                        System.out.printf("*");
+//                }
+//            } else {
+//                for (int j = a; j > 0; j--) {
+//                    if (i - a >= j)
+//                        System.out.printf("*");
+//                    else
+//                        System.out.printf(" ");
+//                }
+//            }
+//            System.out.printf("\n");
+//
+//
+////            for (int j = a; j > 0; j--) {
+////
+////                if(a - j > a)
+////                    System.out.printf("*");
+////                else
+////                    System.out.printf(" ");
+////
+////            }
+//
+//
+//
+//
+////            if (a > i) {
+////
+////                for (int j = a; j > 0; j--) {
+////                    System.out.printf(" ");
+////                }
+////            } else {
+////                for (int j = 0; j < i; j++) {
+////                    System.out.printf("*");
+////                }
+////            }
+//
+////            for (int j = 0; j < a; j++) {
+////                if (j < i)
+////                    System.out.printf(" ");
+////                else
+////                    System.out.printf("*");
+////            }
+//
+//        }
+
+
+
+
+
+
+
+//        for (int i = 0; i < a; i++) {
+//            for (int j = 0; j < a; j++) {
+//                if (i <= j){
+//                    System.out.printf("*");
+//                } else {
+//                    System.out.printf(" ");
+//                }
+//            }
+//            System.out.printf("\n");
+//        }
+
+    }
+
+}

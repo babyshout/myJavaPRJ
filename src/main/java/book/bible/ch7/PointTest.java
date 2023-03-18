151d53e5e2dce0d8cfcc95a3f6a6fb48e117b534
home <lsms0523@gmail.com>
2023-03-18 14:01:55 +0900

    COMMIT

---
+++ src/main/java/book/bible/ch7/PointTest.java
@@ -0,0 +1,33 @@
+//package book.bible.ch7;
+//
+//public class PointTest {
+//    public static void main(String[] args) {
+//        Point3D p3 = new Point3D(1, 2, 3);
+//    }
+//}
+//
+//class Point {
+//    int x;
+//    int y;
+//
+//    Point(int x, int y) {
+//        this.x = x;
+//        this.y = y;
+//    }
+//    String getLocation() {
+//        return "x : " + x + " , y : " + y;
+//    }
+//}
+//
+//class Point3D {
+//    int z;
+//    Point3D(int x, int y, int z) {
+////        super(x, y);
+////        this.x = x;
+////        this.y = y;
+//        this.z = z;
+//    }
+//    String getLocation() {
+//        return "x : " + x + " , y : " + y + ", z : " + z;
+//    }
+//}

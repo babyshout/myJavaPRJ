e061a62158b739957747e025908fef2807039675
babyshout <lsms0523@gmail.com>
2023-03-20 20:47:17 +0900

    Commit 0320 school

--- src/main/java/book/bible/ch7/TVCR.java
+++ src/main/java/book/bible/ch7/TVCR.java
@@ -1,45 +1,45 @@
-package book.bible.ch7;
-
-class Tv {
-    boolean power;
-    int channel;
-
-    void power() { power = !power; }
-    void channelUp() { ++channel; }
-    void channelDown() { --channel; }
-}
-
-class VCR {
-    boolean power;
-    int counter = 0;
-    void power() { power = !power; }
-    void play() {
-        System.out.println("play");
-    }
-    void stop() {
-        System.out.println("stop");
-    }
-    void rew() {
-        System.out.println("rew");
-    }
-    void ff() {
-        System.out.println("ff");
-    }
-}
-public class TVCR extends Tv {
-    VCR vcr = new VCR();
-    int counter = vcr.counter;
-
-    void play() {
-        vcr.play();
-    }
-    void stop() {
-        vcr.stop();
-    }
-    void rew() {
-        vcr.rew();
-    }
-    void ff() {
-        vcr.ff();
-    }
-}
+//package book.bible.ch7;
+//
+//class Tv {
+//    boolean power;
+//    int channel;
+//
+//    void power() { power = !power; }
+//    void channelUp() { ++channel; }
+//    void channelDown() { --channel; }
+//}
+//
+//class VCR {
+//    boolean power;
+//    int counter = 0;
+//    void power() { power = !power; }
+//    void play() {
+//        System.out.println("play");
+//    }
+//    void stop() {
+//        System.out.println("stop");
+//    }
+//    void rew() {
+//        System.out.println("rew");
+//    }
+//    void ff() {
+//        System.out.println("ff");
+//    }
+//}
+//public class TVCR extends Tv {
+//    VCR vcr = new VCR();
+//    int counter = vcr.counter;
+//
+//    void play() {
+//        vcr.play();
+//    }
+//    void stop() {
+//        vcr.stop();
+//    }
+//    void rew() {
+//        vcr.rew();
+//    }
+//    void ff() {
+//        vcr.ff();
+//    }
+//}

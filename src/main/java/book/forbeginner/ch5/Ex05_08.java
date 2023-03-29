e3c8abc36f295cb83df31bdaf76aa155749a1a61
babyshout <lsms0523@gmail.com>
2023-03-29 12:31:26 +0900

    Commit 0329 school

---
+++ src/main/java/book/forbeginner/ch5/Ex05_08.java
@@ -0,0 +1,32 @@
+package book.forbeginner.ch5;
+
+import java.util.Scanner;
+
+public class Ex05_08 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+
+        System.out.printf("점수를 입력하세요 : ");
+        a = sc.nextInt();
+
+        if (a >= 90) {
+            System.out.printf("A");
+        } else {
+            if (a >= 80) {
+                System.out.printf("B");
+            } else {
+                if (a >= 70) {
+                    System.out.printf("C");
+                } else {
+                    if (a >= 60) {
+                        System.out.printf("D");
+                    } else {
+                        System.out.printf("F");
+                    }
+                }
+            }
+        }
+        System.out.printf(" 학점 입니다. \n");
+    }
+}
---
+++ src/main/java/book/forbeginner/ch6/Ex06_07.java
@@ -0,0 +1,14 @@
+package book.forbeginner.ch6;
+
+public class Ex06_07 {
+    public static void main(String[] args) {
+        int hap;
+        int i;
+
+        for (i = 1; i < 10; i++) {
+//            hap = hap + i;
+        }
+
+//        System.out.printf("1에서 10 까지의 합 : %d \n", hap);
+    }
+}

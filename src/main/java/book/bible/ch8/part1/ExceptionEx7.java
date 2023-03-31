d579f02f6f8e524ad66cb18be618999195b4c15e
babyshout <lsms0523@gmail.com>
2023-03-31 18:50:26 +0900

    Commit 0331 school

---
+++ src/main/java/book/bible/ch8/part1/ExceptionEx7.java
@@ -0,0 +1,7 @@
+package book.bible.ch8.part1;
+
+public class ExceptionEx7 {
+    public static void main(String[] args) {
+//        throw new Exception();    //Exception을 강제로 발생(오류발생!)(컴파일도 안됨)
+    }
+}
---
+++ src/main/java/book/bible/ch8/part1/ExceptionEx8.java
@@ -0,0 +1,11 @@
+package book.bible.ch8.part1;
+
+public class ExceptionEx8 {
+    public static void main(String[] args) {
+        try {
+            throw new Exception();
+        } catch (Exception e) {
+            System.out.println("Exception이 발생했습니다.");
+        }
+    }
+}

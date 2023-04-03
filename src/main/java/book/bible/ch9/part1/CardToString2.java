13548abb1af4af0871b1eaea2990231a5b9c2f1f
babyshout <lsms0523@gmail.com>
2023-04-03 21:49:35 +0900

    Study 0403 school java.lang package Object class examples equals, hashCode, toString, clone equals : 같은지 검사, 참조변수의 주소값이 같은지 검사 내용이 같은지 알고싶으면 오버라이딩 hashCode : 해쉬코드 검사, 객체의 해쉬코드 값 검사 보통 equals 오버라이딩 하면서 같이 오버라이딩 toString : 객체를 String 으로 바꿔줌 System.out.println(객체); <= 이럴때 씀 clone : 객체를 복제, 단순히 멤버변수 값만 복제함, 따라서 배열이나 인스턴스를 복제해주기 위해선 오버라이딩 필요 (배열이나 인스턴스는 참조변수기 때문에 같은 주소값을 갖게 됨) 인스턴스 복제는 데이터를 복제하는 것이기 때문에 데이터를 보호하기 위해서 <= 이해안됨 Cloneable 인터페이스를 구현한 클래스의 인스턴스(객체)만 clone()을 통한 복제가 가능

---
+++ src/main/java/book/bible/ch9/part1/CardToString2.java
@@ -0,0 +1,28 @@
+package book.bible.ch9.part1;
+
+class Card {
+    String kind;
+    int number;
+    
+    Card() {
+        this("SPADE", 1);
+    }
+    
+    Card(String kind, int number) {
+        this.kind = kind;
+        this.number = number;
+    }
+    
+    public String toString() {
+        // Card 인스턴스의 kind 와 number 를 문자열로 반환한다.
+        return "kind : " + kind + ", number : " + number;
+    }
+}
+
+public class CardToString2 {
+    public static void main(String[] args) {
+        Card c = new Card("HEART", 10);
+        System.out.println(c.toString());
+        System.out.println(c);
+    }
+}
---
+++ src/main/java/book/bible/ch9/part1/ToStringTest.java
@@ -0,0 +1,13 @@
+package book.bible.ch9.part1;
+
+public class ToStringTest {
+    public static void main(String[] args) {
+        String str = new String("KOREA");
+        java.util.Date today = new java.util.Date();
+    
+        System.out.println(str);
+        System.out.println(str.toString());
+        System.out.println(today);
+        System.out.println(today.toString());
+    }
+}

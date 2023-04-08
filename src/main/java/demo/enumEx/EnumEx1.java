99b8dc0cf8fc67b167d421c3e57d90a5d41b8551
babyshout <lsms0523@gmail.com>
2023-04-08 22:35:32 +0900

    Study 04.08 home Enum from google

---
+++ src/main/java/demo/enumEx/EnumEx1.java
@@ -0,0 +1,26 @@
+package demo.enumEx;
+
+public class EnumEx1 {
+    public static final String MALE = "MALE";
+    public static final String FEMALE = "FEMALE";
+
+
+    public static void main(String[] args) {
+        String gender1;
+        gender1 = EnumEx1.MALE;
+        gender1 = EnumEx1.FEMALE;
+        // MALE, FEMALE이 아닌 상수값이 할당 될 때 //컴파일시 에러가 나지 않음. -> 문제점 발생.
+        gender1 = "boy";
+        Gender gender2;
+        gender2 = Gender.MALE;
+        gender2 = Gender.FEMALE;
+        //컴파일 시 의도하지 않는 상수 값을 체크할 수 있음.
+        //Enum으로 정의한 상수값만 할당 받을 수 있음.
+//        gender2 = "boy";    //그러므로 에러!
+    }
+}
+
+enum Gender {
+    MALE, FEMALE;
+
+}

81b44334bdcbdf6599ef9650ce99867db1357234
babyshout <lsms0523@gmail.com>
2023-06-02 12:56:06 +0900

    Study 0601 in school Annotation 어노테이션으로 메서드 앞뒤에 메서드가 시작,종료 했다는걸 알려주는 코드 삽입하기 1일차

---
+++ src/main/java/quiz/month05/day0517extends/CalcChild.java
@@ -0,0 +1,10 @@
+package quiz.month05.day0517extends;
+
+public class CalcChild extends Calc {
+    
+    
+    @Override
+    protected int sum(int a, int b) {
+        return super.sum(a, b);
+    }
+}

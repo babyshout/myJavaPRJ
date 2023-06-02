cbec91bc3ed9600a9bb443ea44904e69cb40107d
babyshout <lsms0523@gmail.com>
2023-06-03 01:28:44 +0900

    home

---
+++ src/main/java/book/bible3rd/ch13/ThreadEx10.java
@@ -0,0 +1,48 @@
+package book.bible3rd.ch13;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class ThreadEx10 implements Runnable {
+    static boolean autoSave = false;
+    
+    public static void main(String[] args) {
+        Thread t = new Thread(new ThreadEx10());
+        t.setDaemon(true);  //이 부분이 없으면 종료되지 않는다.
+        t.start();
+        
+        for (int i = 0; i < 10; i++) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(i);
+            
+            if (i == 5)
+                autoSave = true;
+        }
+        
+        System.out.println("프로그램을 종료합니다.");
+    }
+    
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                Thread.sleep(3 * 1000); //3초마다
+                
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            
+            //autoSave의 값이 true이면 autoSave()를 호출한다.
+            if (autoSave) {
+                autoSave();
+            }
+        }
+    }
+    
+    public void autoSave() {
+        System.out.println("작업파일이 자동저장되었습니다.");
+    }
+}

1968449c00f89bd8dff5eddeebcba6f817fa8b83
babyshout <lsms0523@gmail.com>
2023-09-01 23:27:55 +0900

    home 0901 IteratorEx1.java

---
+++ src/main/java/book/bible3rd/ch11/Iterator/IteratorEx1.java
@@ -0,0 +1,20 @@
+package book.bible3rd.ch11.Iterator;
+
+import java.util.*;
+
+class IteratorEx1 {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+        list.add("5");
+
+        Iterator it = list.iterator();
+        while (it.hasNext()) {
+            Object obj = it.next();
+            System.out.println(obj);
+        }
+    } // main
+}

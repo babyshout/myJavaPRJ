1d7005ce41b0cd52cc44bb1a5dc5d424806046ee
babyshout <lsms0523@gmail.com>
2023-10-20 23:51:25 +0900

    home 1020 StreamEx3.java

--- src/main/java/book/bible3rd/ch14/stream/StreamEx1.java
+++ src/main/java/book/bible3rd/ch14/stream/StreamEx1.java
@@ -5,28 +5,28 @@ import java.util.stream.*;
 
 class StreamEx1 {
     public static void main(String[] args) {
-        Stream<Student> studentStream = Stream.of(
-                new Student("이자바", 3, 300),
-                new Student("김자바", 1, 200),
-                new Student("안자바", 2, 100),
-                new Student("박자바", 2, 150),
-                new Student("소자바", 1, 200),
-                new Student("나자바", 3, 290),
-                new Student("감자바", 3, 180)
+        Stream<Student01> studentStream = Stream.of(
+                new Student01("이자바", 3, 300),
+                new Student01("김자바", 1, 200),
+                new Student01("안자바", 2, 100),
+                new Student01("박자바", 2, 150),
+                new Student01("소자바", 1, 200),
+                new Student01("나자바", 3, 290),
+                new Student01("감자바", 3, 180)
         );
 
-        studentStream.sorted(Comparator.comparing(Student::getBan) // 반별 정렬
+        studentStream.sorted(Comparator.comparing(Student01::getBan) // 반별 정렬
                         .thenComparing(Comparator.naturalOrder()))    // 기본 정렬
                 .forEach(System.out::println);
     }
 }
 
-class Student implements Comparable<Student> {
+class Student01 implements Comparable<Student01> {
     String name;
     int ban;
     int totalScore;
 
-    Student(String name, int ban, int totalScore) {
+    Student01(String name, int ban, int totalScore) {
         this.name =name;
         this.ban =ban;
         this.totalScore =totalScore;
@@ -41,7 +41,7 @@ class Student implements Comparable<Student> {
     int getTotalScore()  { return totalScore;}
 
     // 총점 내림차순을 기본 정렬로 한다.
-    public int compareTo(Student s) {
+    public int compareTo(Student01 s) {
         return s.totalScore - this.totalScore;
     }
 }
---
+++ src/main/java/book/bible3rd/ch14/stream/StreamEx3.java
@@ -0,0 +1,67 @@
+package book.bible3rd.ch14.stream;
+
+
+import java.util.*;
+import java.util.stream.*;
+
+class StreamEx3 {
+    public static void main(String[] args) {
+        Student03[] stuArr = {
+                new Student03("이자바", 3, 300),
+                new Student03("김자바", 1, 200),
+                new Student03("안자바", 2, 100),
+                new Student03("박자바", 2, 150),
+                new Student03("소자바", 1, 200),
+                new Student03("나자바", 3, 290),
+                new Student03("감자바", 3, 180)
+        };
+
+        Stream<Student03> stuStream = Stream.of(stuArr);
+
+        stuStream.sorted(Comparator.comparing(Student03::getBan)
+                        .thenComparing(Comparator.naturalOrder()))
+                .forEach(System.out::println);
+
+        stuStream = Stream.of(stuArr); // 스트림을 다시 생성한다.
+        IntStream stuScoreStream = stuStream.mapToInt(Student03::getTotalScore);
+
+        IntSummaryStatistics stat = stuScoreStream.summaryStatistics();
+        System.out.println("count=" + stat.getCount());
+        System.out.println("sum=" + stat.getSum());
+        System.out.printf("average=%.2f%n", stat.getAverage());
+        System.out.println("min=" + stat.getMin());
+        System.out.println("max=" + stat.getMax());
+    }
+}
+
+class Student03 implements Comparable<Student03> {
+    String name;
+    int ban;
+    int totalScore;
+
+    Student03(String name, int ban, int totalScore) {
+        this.name = name;
+        this.ban = ban;
+        this.totalScore = totalScore;
+    }
+
+    public String toString() {
+        return String.format("[%s, %d, %d]", name, ban, totalScore).toString();
+    }
+
+    String getName() {
+        return name;
+    }
+
+    int getBan() {
+        return ban;
+    }
+
+    int getTotalScore() {
+        return totalScore;
+    }
+
+    public int compareTo(Student03 s) {
+        return s.totalScore - this.totalScore;
+    }
+}
